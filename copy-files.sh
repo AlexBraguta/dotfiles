dd096610df884e2c7c13d1e8e44fdf828fe8cb24
Alex <alexbraguta87@gmail.com>
2025-06-18 23:38:33 +0300

    .

---
+++ copy-files.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cp ~/.config/darkman/config.yaml /mnt/diskD/Projects/dotfiles/darkman/config.yaml
+cp ~/.config/dunst/dunstrc /mnt/diskD/Projects/dotfiles/dunst/dunstrc
+cp ~/.config/hypr/hyprland.conf /mnt/diskD/Projects/dotfiles/hypr/hyprland.conf
+cp ~/.config/hypr/hyprland.conf.save /mnt/diskD/Projects/dotfiles/hypr/hyprland.conf.save
+cp ~/.config/hypr/hyprpaper.conf /mnt/diskD/Projects/dotfiles/hypr/hyprpaper.conf
+cp ~/.config/hypr/hyprpaper-dark.conf /mnt/diskD/Projects/dotfiles/hypr/hyprpaper-dark.conf
+cp ~/.config/hypr/hyprpaper-light.conf /mnt/diskD/Projects/dotfiles/hypr/hyprpaper-light.conf
+cp ~/.config/hypr/startup.sh /mnt/diskD/Projects/dotfiles/hypr/startup.sh
+cp ~/.config/kitty/* /mnt/diskD/Projects/dotfiles/kitty/
+cp ~/.config/mako/* /mnt/diskD/Projects/dotfiles/mako/
+cp ~/.config/meteo-qt/* /mnt/diskD/Projects/dotfiles/meteo-qt/
+cp -r ~/.config/rofi/* /mnt/diskD/Projects/dotfiles/rofi/
+cp -r ~/.config/starship/* /mnt/diskD/Projects/dotfiles/starship/
+cp ~/.config/starship.toml /mnt/diskD/Projects/dotfiles/starship/starship.toml
+cp /usr/share/dark-mode.d/* /mnt/diskD/Projects/dotfiles/usr/share/dark-mode.d/
+cp /usr/share/light-mode.d/* /mnt/diskD/Projects/dotfiles/usr/share/light-mode.d/
+echo "Dotfiles backup complete."
